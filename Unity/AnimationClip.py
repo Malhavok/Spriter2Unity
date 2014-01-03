10558334b1ea7195c53dddc57ac7ef4ac4bb7164
Krzysztof Kalinowski <malhavok@wow.com>
2014-01-03 16:16:59 +0100

    [AnimationClip] animating positions using new TSRCurveWorker
    + replaced old function with new, simpler and nicer implementation

--- Unity/AnimationClip.py
+++ Unity/AnimationClip.py
@@ -11,6 +11,10 @@ from AnimationEvent import AnimationEvent
 
 from MB_AssignSprite import MB_AssignSprite
 
+
+from TSRCurveWorker import TSRCurveWorker
+
+
 import codecs
 
 
@@ -178,36 +182,24 @@ class AnimationClip(object):
         if len(self.keyframes) == 0:
             return None
 
-        cc = CurveCalc()
+        tsrWork = TSRCurveWorker(
+            [('x', TSRCurveWorker.LINEAR), ('y', TSRCurveWorker.LINEAR), ('z', TSRCurveWorker.INSTANT)],
+            'm_LocalPosition'
+        )
 
         for t in self.keyframes.keys():
             for go in self.keyframes[t]:
                 transform = go.get_component_of_type(Transform.type)
-                pos = transform.get_position()
+                tsrWork.add_key_frame(t, go.get_path(), 'z', transform.get_z_position())
+
                 if not go.does_take_part_in_anim_calcs():
-                    # ok, this is important
-                    # even thou time marker for sprite doesn't match it doesn't mean that
-                    # Z position shouldn't be taken into account.
-                    # this is the weirdest bug i have to fix and i'd say
-                    # !!! i'd be good to rewrite most of this code before going further
-                    cc.add_info(go.get_path(), t, (None, None, pos[2]))
                     continue
-                cc.add_info(go.get_path(), t, pos)
 
-        finalKey = self.keyframes[sorted(self.keyframes.keys())[-1]]
-        if self.isLooped:
-            finalKey = self.keyframes[sorted(self.keyframes.keys())[0]]
-
-        for go in finalKey:
-            transform = go.get_component_of_type(Transform.type)
-            pos = transform.get_position()
-            if not go.does_take_part_in_anim_calcs():
-                cc.add_info(go.get_path(), self.animTime, (None, None, pos[2]))
-                continue
-            cc.add_info(go.get_path(), self.animTime, pos)
+                tsrWork.add_key_frame(t, go.get_path(), 'x', transform.get_x_position())
+                tsrWork.add_key_frame(t, go.get_path(), 'y', transform.get_y_position())
 
         # string to write it down, data for editor curves
-        return cc.to_string(), cc.to_editor_string(Transform.type, 'm_LocalPosition')
+        return tsrWork.to_string(), tsrWork.to_editor_string()
 
     def calc_rotation_curves(self):
         if len(self.keyframes) == 0:
