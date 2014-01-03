c88269133b08206c8fe67ea40558fecff7d5469c
Krzysztof Kalinowski <malhavok@wow.com>
2014-01-03 09:34:42 +0100

    [File] default pivot y
    + pivot_y have to be 1.0 by default, despite what can be seen in spriter

--- Spriter/File.py
+++ Spriter/File.py
@@ -23,7 +23,7 @@ class File(object):
         self.width = int(node.attrib['width'])
         self.height = int(node.attrib['height'])
         self.pivot_x = float(node.attrib.get('pivot_x', 0.0))
-        self.pivot_y = float(node.attrib.get('pivot_y', 0.0))
+        self.pivot_y = float(node.attrib.get('pivot_y', 1.0))
 
     def __str__(self):
         return 'File (id: %d, name: %s, w: %d, h: %d, px: %f, py: %f)'\
